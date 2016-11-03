6823e4ad6702062df7139f5b56da16083f80e47f
Sam Stokes <me@samstokes.co.uk>
2016-11-02 21:12:14 -0700

    Add phone number field to contacts table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161102192215) do
+ActiveRecord::Schema.define(version: 20161103041135) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 20161102192215) do
     t.text     "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text     "phone"
   end
 
 end
